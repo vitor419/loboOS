b31b02076ac2ab8c376f69548a77c2c0495707a6
Vitor <vitorpacheco419@gmail.com>
2020-10-04 20:06:25 -0300

    node js

--- src/Databases/index.js
+++ src/Databases/index.js
@@ -1,6 +1,6 @@
 const mogoose = require('mongoose');
 
-mogoose.connect('mongodb://localhost/users', { useNewUrlParse: true});
+mogoose.connect('mongodb://localhost:27017/login', { useNewUrlParse: true});
 mogoose.Promise = global.Promise;
 
 module.exports = mogoose;
